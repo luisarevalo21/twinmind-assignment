25cbc7eb00721f29775f722ba3dd5543ef3e7d04
luisarevalo21 <arevaloluis89@yahoo.com>
2025-05-26 22:41:10 -0700

    adjusted frontned api call

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "http://localhost:3000/";
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:3000/";
 const api = axios.create({
   baseURL,
   headers: {
